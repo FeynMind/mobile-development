643b815749201847ea3272daec0a486379051d80
ridwancy <cyaliandi@apps.ipb.ac.id>
2024-12-08 12:35:58 +0700

    Fix button starter

--- app/src/main/java/com/bangkit/feynmind/ui/onboarding/MainActivity.kt
+++ app/src/main/java/com/bangkit/feynmind/ui/onboarding/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.bangkit.feynmind.R
+import com.bangkit.feynmind.starter.StarterActivity
 import com.bangkit.feynmind.ui.home.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
@@ -20,6 +21,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = getSharedPreferences("FeynMindPreferences", MODE_PRIVATE)
+        val isFirstLaunch = sharedPref.getBoolean("isFirstLaunch", true)
+
+        if (isFirstLaunch) {
+            val intent = Intent(this, StarterActivity::class.java)
+            startActivity(intent)
+            sharedPref.edit().putBoolean("isFirstLaunch", false).apply()
+            finish()
+            return
+        }
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUserLoginStatus()
