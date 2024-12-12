b2c686d8d54f32aafed9feb7bad477d7a73f4c4d
ridwancy <cyaliandi@apps.ipb.ac.id>
2024-12-13 00:58:30 +0700

    home feature

--- app/src/main/java/com/bangkit/feynmind/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/feynmind/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.bangkit.feynmind.ui.home
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -29,9 +30,34 @@ class HomeFragment : Fragment() {
 
         auth = FirebaseAuth.getInstance()
 
+        val sharedPreferences = requireContext().getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        val isWelcomeShown = sharedPreferences.getBoolean("isWelcomeShown", false)
         val currentUser = auth.currentUser
         currentUser?.let {
-            Toast.makeText(requireContext(), "Welcome, ${it.email}", Toast.LENGTH_SHORT).show()
+            // Set nama pengguna di TextView
+            binding.userTv.text = it.displayName ?: "Pengguna"
+        }
+        // Tampilkan pesan hanya jika belum pernah ditampilkan sebelumnya
+        if (!isWelcomeShown) {
+            currentUser?.let {
+                Toast.makeText(requireContext(), "Selamat Datang, ${it.displayName}", Toast.LENGTH_SHORT).show()
+            }
+
+            // Simpan status bahwa pesan sudah ditampilkan
+            sharedPreferences.edit().putBoolean("isWelcomeShown", true).apply()
+        }
+
+        // Tambahkan aksi pada ImageButton
+        binding.quizBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Fitur ini sedang dalam tahap pengembangan", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.summaryBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Fitur ini sedang dalam tahap pengembangan", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.tipsBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Fitur ini sedang dalam tahap pengembangan", Toast.LENGTH_SHORT).show()
         }
 
         binding.btnLogout.setOnClickListener {
@@ -40,10 +66,13 @@ class HomeFragment : Fragment() {
     }
 
     private fun logoutUser() {
+        val sharedPreferences = requireContext().getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        sharedPreferences.edit().putBoolean("isWelcomeShown", false).apply() // Reset status
+
         auth.signOut()
 
         val intent = Intent(requireContext(), LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK // Clear back stack
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
         activity?.finish()
     }
